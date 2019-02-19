c3114fa73b587521a4ed76beafe123117d8a0496
AlexSaplin <40490358+AlexSaplin@users.noreply.github.com>
2019-02-20 02:27:59 +0300

    fix: removed tabs added spaces

--- TASK_1/tar_backup_script.sh
+++ TASK_1/tar_backup_script.sh
@@ -9,29 +9,29 @@ shift 2
 files=()
 
 for extension in $@; do
-	files+=$(find $HOME -name "*."$extension)
-	files+=$'\n'
+    files+=$(find . -name "*."$extension)
+    files+=$'\n'
 done
 
 mkdir $dir_name
 
 for file in $files; do
-	basefile=$(basename -- "$file")
+    basefile=$(basename -- "$file")
 
-	if [[ -f $dir_name"/"$basefile ]]; then
-			cnt=1
-			
-			filename="${basefile%.*}"
-			extension="${basefile##*.}"
-			
-			while [[ -f $dir_name"/"$filename"("$cnt")."$extension ]]; do
-				cnt=$(( $cnt+1 ))
-			done
-			
-			basefile=$filename"("$cnt")."$extension
-		fi
-	
-	cp $file $dir_name/$basefile
+    if [[ -f $dir_name"/"$basefile ]]; then
+            cnt=1
+            
+            filename="${basefile%.*}"
+            extension="${basefile##*.}"
+            
+            while [[ -f $dir_name"/"$filename"("$cnt")."$extension ]]; do
+                cnt=$(( $cnt+1 ))
+            done
+            
+            basefile=$filename"("$cnt")."$extension
+        fi
+    
+    cp $file $dir_name/$basefile
 done
 
 tar -czf $zip_name".tar.gz" $dir_name &> /dev/null
